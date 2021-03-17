978a6384fed60a4965b5d6e35aae9d535a69b34c
snowbeauti <snowbeauti@naver.com>
2021-03-17 10:52:08 +0900

    서비스변경

--- src/main/java/com/javaex/service/ProdService.java
+++ src/main/java/com/javaex/service/ProdService.java
@@ -3,7 +3,7 @@ package com.javaex.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.javaex.dao.ProductDao;
+import com.javaex.dao.ProdDao;
 import com.javaex.vo.ProductVo;
 
 @Service
